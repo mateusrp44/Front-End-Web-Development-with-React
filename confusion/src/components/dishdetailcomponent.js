29626349d98e0894d5c2881d9cf5a9dd276ffaf8
Mateus Rocha Pinto <mateusrpinto44@gmail.com>
2019-08-22 11:29:22 -0300

    Fetch from Server

--- confusion/src/components/dishdetailcomponent.js
+++ confusion/src/components/dishdetailcomponent.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 import { Loading } from './loadingcomponent';
+import { baseUrl } from '../shared/baseurl';
 
 function RenderDish({ dish }) {
     return (
         <Card>
-            <CardImg width="100%" src={ dish.image } alt={ dish.name } />
+            <CardImg width="100%" src={ baseUrl + dish.image } alt={ dish.name } />
             <CardBody>
                 <CardTitle>{ dish.name }</CardTitle>
                 <CardText>{ dish.description }</CardText>
--- confusion/src/components/maincomponent.js
+++ confusion/src/components/maincomponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/actioncreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/actioncreators';
 import { actions } from 'react-redux-form';
 
 import Home from './homecomponent';
@@ -13,11 +13,12 @@ import DishDetail from './dishdetailcomponent';
 import About from './aboutcomponent';
 
 const mapDispatchToProps = dispatch => ({
-    addComment: (dishId, rating, author, comment) => dispatch(
-        addComment(dishId, rating, author, comment)),
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => { dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
-});
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos())
+  });
 
 const mapStateToProps = state => {
     return {
@@ -31,16 +32,20 @@ const mapStateToProps = state => {
 class Main extends Component {
     componentDidMount() {
         this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
     }
 
     render() {
         const HomePage = () => {
             return (
-                <Home
+                <Home 
                     dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
                     dishesLoading={this.props.dishes.isLoading}
-                    dishesErrMess={this.props.dishes.errMess}
-                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    dishErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    promoLoading={this.props.promotions.isLoading}
+                    promoErrMess={this.props.promotions.errMess}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
@@ -51,7 +56,8 @@ class Main extends Component {
                 <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                     isLoading={this.props.dishes.isLoading}
                     errMess={this.props.dishes.errMess}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                    commentsErrMess={this.props.comments.errMess}
                     addComment={this.props.addComment}
                 />
             );
